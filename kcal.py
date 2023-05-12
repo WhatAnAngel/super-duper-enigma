b0da4f9e1666c1f1d65c86149a46fc3ce83d4f94
WhatAnAngel <117033853+WhatAnAngel@users.noreply.github.com>
2023-05-12 20:14:52 +0200

    Update kcal.py

--- kcal.py
+++ kcal.py
@@ -65,7 +65,7 @@ def obtener_nivel_actividad():
 
 nivel_actividad = obtener_nivel_actividad()
 
-def calcular_IMC(peso, altura):
+def calcular_imc(peso, altura):
     altura_en_metros = altura / 100
     imc = peso / (altura_en_metros ** 2)
     if imc < 18.5:
@@ -81,29 +81,29 @@ def calcular_IMC(peso, altura):
     else:
         return "Obesidad tipo III"
 
-imc = calcular_IMC(peso, altura)
+IMC = calcular_imc(peso, altura)
 
 def calcular_gasto_energetico(peso, altura, sexo, edad, nivel_actividad, imc):
     if imc.startswith("Obesidad"):
         if sexo == 1:  # Masculino
-            mb = (10 * peso) + (6.25 * altura) - (5 * edad) + 5
+            gasto_energetico_basal = (10 * peso) + (6.25 * altura) - (5 * edad) + 5
         else:  # Femenino
-            mb = (10 * peso) + (6.25 * altura) - (5 * edad) - 161
+            gasto_energetico_basal = (10 * peso) + (6.25 * altura) - (5 * edad) - 161
     else:
         if sexo == 1:  # Masculino
-            mb = 66.5 + (13.75 * peso) + (5.003 * altura) - (6.755 * edad)
+            gasto_energetico_basal = 66.5 + (13.75 * peso) + (5.003 * altura) - (6.755 * edad)
         else:  # Femenino
-            mb = 655.1 + (9.563 * peso) + (1.85 * altura) - (4.676 * edad)
+            gasto_energetico_basal = 655.1 + (9.563 * peso) + (1.85 * altura) - (4.676 * edad)
 
     factores_actividad = [1.2, 1.375, 1.55, 1.725, 1.9]
     factor = factores_actividad[nivel_actividad]
-    gasto_energetico_total = mb * factor
+    gasto_energetico_total = gasto_energetico_basal * factor
 
-    return mb, gasto_energetico_total
+    return gasto_energetico_basal, gasto_energetico_total
 
 def calcular_calorias(peso, altura, sexo, edad, nivel_actividad):
-    imc = calcular_IMC(peso, altura)
-    mb, gasto_energetico_total = calcular_gasto_energetico(peso, altura, sexo, edad, nivel_actividad, imc)
+    imc = calcular_imc(peso, altura)
+    gasto_energetico_basal, gasto_energetico_total = calcular_gasto_energetico(peso, altura, sexo, edad, nivel_actividad, imc)
 
     if imc.startswith("Obesidad"):
         print("Debido a que el IMC indica obesidad, el objetivo se restringe a pérdida de peso.")
@@ -128,7 +128,23 @@ def calcular_calorias(peso, altura, sexo, edad, nivel_actividad):
             calorias = gasto_energetico_total - 500  # Pérdida de peso moderada
         else:
             calorias = gasto_energetico_total - 700  # Pérdida de peso severa
+
     else:
+        print("Opciones de objetivo:")
+        print("1: Pérdida de peso")
+        print("2: Mantenimiento de peso")
+        print("3: Aumento de peso")
+
+        while True:
+            try:
+                objetivo = int(input("Ingrese su objetivo: "))
+                if objetivo >= 1 and objetivo <= 3:
+                    break
+                else:
+                    print("Opción inválida. Intente nuevamente.")
+            except ValueError:
+                print("Entrada inválida. Intente nuevamente.")
+
         if objetivo == 1:
             calorias = gasto_energetico_total - 500
         elif objetivo == 2:
@@ -140,13 +156,15 @@ def calcular_calorias(peso, altura, sexo, edad, nivel_actividad):
 
     calorias = round(calorias, 2)  # Limitar a 2 decimales
 
-    return imc, mb, gasto_energetico_total, calorias
+    return imc, gasto_energetico_basal, gasto_energetico_total, calorias
 
 # Calcular calorías
-imc, mb, gasto_energetico_total, calorias = calcular_calorias(peso, altura, sexo, edad, nivel_actividad)
+IMC, gasto_energetico_basal, gasto_energetico_total, calorias = calcular_calorias(peso, altura, sexo, edad, nivel_actividad)
+gasto_energetico_basal = round(gasto_energetico_basal, 2)
+gasto_energetico_total = round(gasto_energetico_total, 2)
 
 # Mostrar resultados
-print("Estado nutricional (IMC):", imc)
-print("Gasto energético basal:", mb, "kcal")
+print("Estado nutricional (IMC):", IMC)
+print("Gasto energético basal:", gasto_energetico_basal, "kcal")
 print("Gasto energético total:", gasto_energetico_total, "kcal")
 print("Calorías recomendadas:", calorias, "kcal")
